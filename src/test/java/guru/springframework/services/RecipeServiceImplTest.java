2879372c817f8cadeaf56cc9fd5e874577ef6f97
Jian-Min Gao <jian-min.gao@hpe.com>
2018-07-01 18:01:54 +0800

    using mockito mocks

---
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.annotation.Primary;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        Iterable<Recipe> recipes = recipeService.getRecipes();
+        final AtomicInteger size=new AtomicInteger();
+        recipes.forEach(r -> size.incrementAndGet());
+        assertEquals(size.get(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
