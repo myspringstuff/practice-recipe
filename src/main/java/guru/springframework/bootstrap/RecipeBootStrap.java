e4af54d15d2fed3ea5d38edefc8ce322b24208ea
Jian-Min Gao <jian-min.gao@hpe.com>
2018-07-01 10:34:10 +0800

    added ingredients

--- src/main/java/guru/springframework/bootstrap/RecipeBootStrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootStrap.java
@@ -43,6 +43,23 @@ public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEven
         recipe.setDifficulty(Difficulty.EASY);
         recipe.setSource("Simple Recipes");
         recipe.setUrl("https://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        recipe.setNotes(new Notes("We have a family motto and it is this: Everything goes better in a tortilla.\n" +
+                "\n" +
+                "Any and every kind of leftover can go inside a warm tortilla, usually with a healthy dose of pickled jalapenos. I can always sniff out a late-night snacker when the aroma of tortillas heating in a hot pan on the stove comes wafting through the house.\n" +
+                "\n" +
+                "Today’s tacos are more purposeful – a deliberate meal instead of a secretive midnight snack!\n" +
+                "\n" +
+                "\n" +
+                "First, I marinate the chicken briefly in a spicy paste of ancho chile powder, oregano, cumin, and sweet orange juice while the grill is heating. You can also use this time to prepare the taco toppings.\n" +
+                "\n" +
+                "Grill the chicken, then let it rest while you warm the tortillas. Now you are ready to assemble the tacos and dig in. The whole meal comes together in about 30 minutes!" +
+                "The ancho chiles I use in the marinade are named for their wide shape. They are large, have a deep reddish brown color when dried, and are mild in flavor with just a hint of heat. You can find ancho chile powder at any markets that sell Mexican ingredients, or online.\n" +
+                "\n" +
+                "I like to put all the toppings in little bowls on a big platter at the center of the table: avocados, radishes, tomatoes, red onions, wedges of lime, and a sour cream sauce. I add arugula, as well – this green isn’t traditional for tacos, but we always seem to have some in the fridge and I think it adds a nice green crunch to the tacos.\n" +
+                "\n" +
+                "Everyone can grab a warm tortilla from the pile and make their own tacos just they way they like them.\n" +
+                "\n" +
+                "You could also easily double or even triple this recipe for a larger party. A taco and a cold beer on a warm day? Now that’s living!"));
         recipe.setDirections("1 Prepare a gas or charcoal grill for medium-high, direct heat.\n" +
                 "\n" +
                 "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make a loose paste. Add the chicken to the bowl and toss to coat all over.\n" +
@@ -56,6 +73,16 @@ public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEven
                 "Wrap warmed tortillas in a tea towel to keep them warm until serving.\n" +
                 "\n" +
                 "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges.");
+
+        recipe.addIngredient(new Ingredient(BigDecimal.valueOf(8), null, "small corn tortillas"));
+        recipe.addIngredient(new Ingredient(BigDecimal.valueOf(3), findUom("cup"), "packed baby arugula (3 ounces)"));
+        recipe.addIngredient(new Ingredient(BigDecimal.valueOf(2), null, "medium ripe avocados, sliced"));
+        recipe.addIngredient(new Ingredient(BigDecimal.valueOf(4), null, "radishes, thinly sliced"));
+        recipe.addIngredient(new Ingredient(BigDecimal.valueOf(0.5), findUom("pint"), "cherry tomatoes, halved"));
+        recipe.addIngredient(new Ingredient(BigDecimal.valueOf(0.25), null, "red onion, thinly sliced"));
+        recipe.addIngredient(new Ingredient(BigDecimal.ONE, null, "Roughly chopped cilantro"));
+        recipe.addIngredient(new Ingredient(BigDecimal.valueOf(0.5), findUom("cup"), "sour cream thinned with 1/4 cup milk"));
+        recipe.addIngredient(new Ingredient(BigDecimal.ONE, null, "lime, cut into wedges"));
 
         Category american = categoryRepository.findByDescription("American").get();
         recipe.addCategory(american);
