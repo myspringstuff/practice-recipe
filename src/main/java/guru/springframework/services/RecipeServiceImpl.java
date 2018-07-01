b3e14ae52874b3e092543e08f823133391b5b1a4
Jian-Min Gao <jian-min.gao@hpe.com>
2018-07-01 10:51:42 +0800

    assignment: refactoring to use project Lombok

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,11 +2,12 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private RecipeRepository recipeRepository;
@@ -17,6 +18,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Iterable<Recipe> getRecipes() {
+        log.debug("Getting all recipes via repository....");
         return recipeRepository.findAll();
     }
 }
