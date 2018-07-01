7a64569f160f5b1ca7a26f16a30edf0374138e13
Jian-Min Gao <jian-min.gao@hpe.com>
2018-07-01 19:37:33 +0800

    refactoring for mockito argument captor

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,13 +1,18 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import static org.mockito.Mockito.*;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.*;
 
@@ -27,9 +32,28 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+        //given
+        Iterable<Recipe> recipes = new HashSet<>();
+        ((HashSet<Recipe>) recipes).add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(99L);
+        ((HashSet<Recipe>) recipes).add(recipe);
 
-        assertEquals("index", indexController.getIndexPage(model));
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Iterable<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Iterable.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+        //then
+        assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anyIterable());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Iterable<Recipe> iterableInController = argumentCaptor.getValue();
+        final AtomicInteger size = new AtomicInteger();
+        iterableInController.forEach(r -> size.incrementAndGet());
+        assertEquals(2, size.get());
     }
 }
